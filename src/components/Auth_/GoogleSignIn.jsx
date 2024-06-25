16fe15917c2c7c52aa033b944197851db13ce12d
yoadmash <yoad.mashiach2@gmail.com>
2024-06-25 23:12:15 +0300

    adding google auth

--- src/components/Auth_/GoogleSignIn.jsx
+++ src/components/Auth_/GoogleSignIn.jsx
@@ -47,6 +47,7 @@ const GoogleSignIn = () => {
     useEffect(() => {
         const getRedirectResultEffect = async () => {
             const redirectResult = await getRedirectResult(firebaseAuth);
+            console.log(redirectResult);
             if (redirectResult) {
                 await googleAuth(redirectResult._tokenResponse);
             }
